5d9c8f7973d26df08b07cb8cdf7c487da3350ac5
Iryna Ason <asonirina@mail.ru>
2013-12-12 22:22:21 +0300

    add buttons

---
+++ src/main/java/com/sun/manager/forms/ButtonCell.java
@@ -0,0 +1,27 @@
+package com.sun.manager.forms;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.TableCell;
+
+/**
+ * User: iason
+ */
+public  class ButtonCell<T> extends TableCell<T, String> {
+    final Button cellButton = new Button();
+
+    public ButtonCell(){
+        cellButton.setPrefWidth(190);
+        cellButton.setPrefHeight(10);
+    }
+
+    //Display button if the row is not empty
+    @Override
+    protected void updateItem(String item, boolean empty) {
+        super.updateItem(item, empty);
+        cellButton.setText(item);
+        if(!empty){
+            setGraphic(cellButton);
+        }
+    }
+}
+
